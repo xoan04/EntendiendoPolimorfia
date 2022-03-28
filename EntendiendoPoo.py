b1a50bd63aba55fc20b7b33fde42a9610025a652
xoan04 <80915299+xoan04@users.noreply.github.com>
2022-03-28 11:53:33 -0500

    Dia 1

---
+++ EntendiendoPoo.py
@@ -0,0 +1,36 @@
+def ingresarCad (cad):
+    cad.ingresar()
+def verificarAlfabeto(cadAlfabeto):
+    cadAlfabetoToSet = set(cadAlfabeto)
+    new_list = list(cadAlfabetoToSet)
+    return new_list
+    
+class Alfabeto:    
+
+    def __init__(self, cadena):
+        self.cadAlfabeto = cadena
+        
+
+    def __repr__(self):
+        return str(self.cadAlfabeto)
+
+    def ingresar(self,cadAlfabeto):
+        cadAlfabeto = verificarAlfabeto(cadAlfabeto)
+        
+
+class Lenguaje:
+    def __init__(self, cadenaL):
+        self.cadLenguage = cadena
+        
+
+    def __repr__(self):
+        return str(self.cadLenguage)
+
+main_list = [""]
+Lista_Union=[]
+resultantList = []
+cantidadAlfb = int( input("Ingrese la cantidad de alfabetos que desea: "))
+
+    for i in range (cantidadAlfb):
+        cadAlfabeto = input("ingrese su cadena número "+str(i+1)+" separada por espacios: ").split(" ")
+        ingresar(cadAlfabeto)
